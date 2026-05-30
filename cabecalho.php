815c9770b5abce7ae509ca19de4d2362a676ea6d
nry422 <henrymuller@gmail.com>
2026-05-30 17:12:58 -0300

    correção

--- cabecalho.php
+++ cabecalho.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Contato PHP Profissional</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
