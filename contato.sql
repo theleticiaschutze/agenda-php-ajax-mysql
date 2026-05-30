76d5952cb76328636dd61d7aaf944df5cde03577
nry422 <henrymuller@gmail.com>
2026-05-30 12:45:35 -0300

    script de criação db
    complementado e corrigido

--- contato.sql
+++ contato.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `contato`(
-    `id`
-    `nome`
-    `telefone`
-    `email`
-    `endereco`
-    `instagram`
-    PRIMARY KEY (`id`);
+    `id` int(11) not null auto_increment,
+    `nome` varchar(50) not null,
+    `telefone` varchar(50) null,
+    `email` varchar(50) null,
+    `endereco` text null,
+    `instagram` varchar(100) null,
+    PRIMARY KEY (`id`)
 );
